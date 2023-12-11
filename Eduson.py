ff5ca50f91064736235c0c0b878f340090f8f341
Nadezhda <nadezhda.shastak@gmail.com>
2023-12-11 17:27:53 +0300

    Initial commit test

---
+++ Eduson.py
@@ -0,0 +1,11 @@
+MY_CONSTANT = 'Это результат'
+
+
+def this_is_functional():
+    first_variable = 2
+    second_variable = 4
+    third_variable = first_variable + second_variable
+    return MY_CONSTANT, third_variable
+
+
+print(this_is_functional())
